773b1f4ebe69d8785957b39b681853ff8e4130b2
EC2 Default User <ec2-user@ip-172-31-5-8.ap-northeast-1.compute.internal>
2022-01-05 19:26:26 +0000

    ⑴0を押した後に数字を入力できるように修正⑵００等の０を押した後に０を入力できないように修正⑶等価演算子を厳密等価演算子に修正　　その他バグを修正

--- main.js
+++ main.js
@@ -4,20 +4,22 @@ let textDate = "";
 
 $(document).ready(function(){
   $(".zero").click(function() {
+    if (textDate !== "0" && textDate !== "00"){
         $(".textDate").text(textDate + "0");
         textDate=textDate+"0"
+    }
   });
   
   $(".zerozero").click(function() {
-    if (textDate != ""){
+    if (textDate !== ""){
         $(".textDate").text(textDate + "00");
         textDate=textDate+"00"
     }
   });
   
   $(".point").click(function() {
-    let dontDuplicatePoint= textDate.match(/\./);
-    if (textDate != "" && dontDuplicatePoint== null){
+    let dontDuplicatePoint = textDate.match(/\./);
+    if (textDate !== "" && dontDuplicatePoint === null){
         $(".textDate").text(textDate + ".");
         textDate=textDate+"."
     }
@@ -25,65 +27,101 @@ $(document).ready(function(){
   });
  
   $(".one").click(function() {
-    if (textDate != "0"){
+    if (textDate !== "0"){
       $(".textDate").text(textDate + "1");
       textDate=textDate+"1"
+    }
+    if (textDate === "0"){
+      $(".textDate").text("1");
+      textDate="1"
     }
   });
   
   $(".two").click(function() {
-    if (textDate != "0"){
+    if (textDate !== "0"){
       $(".textDate").text(textDate + "2");
       textDate=textDate+"2"
+    }
+    if (textDate === "0"){
+      $(".textDate").text("2");
+      textDate="2"
     }
   });
   
   $(".three").click(function() {
-    if (textDate != "0"){
+    if (textDate !== "0"){
       $(".textDate").text(textDate + "3");
       textDate=textDate+"3"
+    }
+    if (textDate === "0"){
+      $(".textDate").text("3");
+      textDate="3"
     }
   });
   
   $(".four").click(function() {
-    if (textDate != "0"){
+    if (textDate !== "0"){
       $(".textDate").text(textDate + "4");
       textDate=textDate+"4"
+    }
+    if (textDate === "0"){
+      $(".textDate").text("4");
+      textDate="4"
     }
   });
   
   $(".five").click(function() {
-    if (textDate != "0"){
+    if (textDate !== "0"){
       $(".textDate").text(textDate + "5");
       textDate=textDate+"5"
+    }
+    if (textDate === "0"){
+      $(".textDate").text("5");
+      textDate="5"
     }
   });
   
   $(".six").click(function() {
-    if (textDate != "0"){
+    if (textDate !== "0"){
       $(".textDate").text(textDate + "6");
       textDate=textDate+"6"
+    }
+    if (textDate === "0"){
+      $(".textDate").text("6");
+      textDate="6"
     }
   });
   
   $(".seven").click(function() {
-    if (textDate != "0"){
+    if (textDate !== "0"){
       $(".textDate").text(textDate + "7");
-      textDate=textDate+"9"
+      textDate=textDate+"7"
+    }
+    if (textDate === "0"){
+      $(".textDate").text("7");
+      textDate="7"
     }
   });
   
   $(".eight").click(function() {
-    if (textDate != "0"){
+    if (textDate !== "0"){
       $(".textDate").text(textDate + "8");
       textDate=textDate+"8"
+    }
+    if (textDate === "0"){
+      $(".textDate").text("8");
+      textDate="8"
     }
   });
   
   $(".nine").click(function() {
-    if (textDate != "0"){
+    if (textDate !== "0"){
       $(".textDate").text(textDate + "9");
       textDate=textDate+"9"
+    }
+    if (textDate === "0"){
+      $(".textDate").text("9");
+      textDate="9"
     }
   });
   
@@ -97,40 +135,40 @@ $(document).ready(function(){
     let dontDuplicateMinus= textDate.match(/\-$/);
     let dontDuplicateTimes= textDate.match(/\*$/);
     let dontDuplicateDivided= textDate.match(/\/$/);
-    if (textDate != "" && dontDuplicatePlus== null && dontDuplicateMinus == null && dontDuplicateTimes== null && dontDuplicateDivided== null){
+    if (textDate !== "" && dontDuplicatePlus === null && dontDuplicateMinus === null && dontDuplicateTimes === null && dontDuplicateDivided === null){
         $(".textDate").text(textDate + "+");
         textDate=textDate+"+"
     }
   });
   
   $(".minus").click(function() {
-    let dontDuplicatePlus= textDate.match(/\+$/);
-    let dontDuplicateMinus= textDate.match(/\-$/);
-    let dontDuplicateTimes= textDate.match(/\*$/);
-    let dontDuplicateDivided= textDate.match(/\/$/);
-    if (textDate != "" && dontDuplicatePlus== null && dontDuplicateMinus == null && dontDuplicateTimes== null && dontDuplicateDivided== null){
+    let dontDuplicatePlus = textDate.match(/\+$/);
+    let dontDuplicateMinus = textDate.match(/\-$/);
+    let dontDuplicateTimes = textDate.match(/\*$/);
+    let dontDuplicateDivided = textDate.match(/\/$/);
+    if (textDate !== "" && dontDuplicatePlus === null && dontDuplicateMinus === null && dontDuplicateTimes === null && dontDuplicateDivided === null){
         $(".textDate").text(textDate + "-");
         textDate=textDate+"-"
     }
   });
   
   $(".times").click(function() {
-    let dontDuplicatePlus= textDate.match(/\+$/);
-    let dontDuplicateMinus= textDate.match(/\-$/);
-    let dontDuplicateTimes= textDate.match(/\*$/);
-    let dontDuplicateDivided= textDate.match(/\/$/);
-    if (textDate != "" && dontDuplicatePlus== null && dontDuplicateMinus == null && dontDuplicateTimes== null && dontDuplicateDivided== null){
+    let dontDuplicatePlus = textDate.match(/\+$/);
+    let dontDuplicateMinus = textDate.match(/\-$/);
+    let dontDuplicateTimes = textDate.match(/\*$/);
+    let dontDuplicateDivided = textDate.match(/\/$/);
+    if (textDate !== "" && dontDuplicatePlus === null && dontDuplicateMinus === null && dontDuplicateTimes === null && dontDuplicateDivided === null){
         $(".textDate").text(textDate + "*");
         textDate=textDate+"*"
     }
   });
   
   $(".divided").click(function() {
-    let dontDuplicatePlus= textDate.match(/\+$/);
-    let dontDuplicateMinus= textDate.match(/\-$/);
-    let dontDuplicateTimes= textDate.match(/\*$/);
-    let dontDuplicateDivided= textDate.match(/\/$/);
-    if (textDate != "" && dontDuplicatePlus== null && dontDuplicateMinus == null && dontDuplicateTimes== null && dontDuplicateDivided== null){
+    let dontDuplicatePlus = textDate.match(/\+$/);
+    let dontDuplicateMinus = textDate.match(/\-$/);
+    let dontDuplicateTimes = textDate.match(/\*$/);
+    let dontDuplicateDivided = textDate.match(/\/$/);
+    if (textDate !== "" && dontDuplicatePlus === null && dontDuplicateMinus === null && dontDuplicateTimes === null && dontDuplicateDivided === null){
         $(".textDate").text(textDate + "/");
         textDate=textDate+"/"
     }
